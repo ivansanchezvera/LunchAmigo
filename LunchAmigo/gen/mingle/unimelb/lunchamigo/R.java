dc739570e43f9a3297855d690fb13438c3ec6e0a
denisthamrin@gmail.com <denisthamrin@gmail.com>
2014-03-22 18:36:50 +1100

    Created LoginActivity.java , activity_login.xml 		OptionGateway.java , activity_option_gateway.xml xml is already set, provided a "main function" in the main activity where you can try and see the java file I created

--- LunchAmigo/gen/mingle/unimelb/lunchamigo/R.java
+++ LunchAmigo/gen/mingle/unimelb/lunchamigo/R.java
@@ -24,21 +24,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_forgot_password=0x7f08000e;
-        public static final int action_settings=0x7f08000f;
-        public static final int button1=0x7f08000b;
-        public static final int button2=0x7f08000c;
-        public static final int button3=0x7f08000d;
+        public static final int action_forgot_password=0x7f08000a;
+        public static final int action_settings=0x7f08000b;
+        public static final int activityButton=0x7f080008;
+        public static final int button3=0x7f080009;
         public static final int email=0x7f080003;
         public static final int login=0x7f080005;
+        public static final int loginButton=0x7f080007;
         public static final int login_form=0x7f080002;
         public static final int login_status=0x7f080000;
         public static final int login_status_message=0x7f080001;
         public static final int password=0x7f080004;
-        public static final int progressBar1=0x7f080008;
-        public static final int progressBar2=0x7f080009;
-        public static final int ratingBar1=0x7f080007;
-        public static final int ratingBar2=0x7f08000a;
         public static final int sign_in_button=0x7f080006;
     }
     public static final class layout {
--- LunchAmigo/src/mingle/unimelb/lunchamigo/MainActivity.java
+++ LunchAmigo/src/mingle/unimelb/lunchamigo/MainActivity.java
@@ -3,6 +3,8 @@ package mingle.unimelb.lunchamigo;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends Activity {
 
@@ -18,5 +20,15 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
+	public void goToLogin(View view) {
+	    // Do something in response to button
+		Intent intent = new Intent(this, LoginActivity.class);
+		startActivity(intent);
+	}
+	
+	public void goToOption(View view) {
+		Intent intent = new Intent(this, OptionGateway.class);// Do something in response to button
+		startActivity(intent);
+	}
 }
