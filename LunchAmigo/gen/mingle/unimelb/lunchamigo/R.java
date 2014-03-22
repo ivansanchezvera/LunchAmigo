1ef9283101fa310b4ccd4e9b631faa93db5a7162
denisthamrin@gmail.com <denisthamrin@gmail.com>
2014-03-22 18:12:10 +1100

    added option_gateway

--- LunchAmigo/gen/mingle/unimelb/lunchamigo/R.java
+++ LunchAmigo/gen/mingle/unimelb/lunchamigo/R.java
@@ -24,7 +24,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
+        public static final int action_settings=0x7f080007;
+        public static final int button1=0x7f080004;
+        public static final int button2=0x7f080005;
+        public static final int button3=0x7f080006;
         public static final int progressBar1=0x7f080001;
         public static final int progressBar2=0x7f080002;
         public static final int ratingBar1=0x7f080000;
@@ -32,6 +35,7 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int option_gateway=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
