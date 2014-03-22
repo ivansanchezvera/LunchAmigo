fa12a535740aa40aeb868f2bfec58136f2d2ef50
denisthamrin@gmail.com <denisthamrin@gmail.com>
2014-03-22 19:41:46 +1100

    All going good, the deleted changes are now back

--- LunchAmigo/gen/mingle/unimelb/lunchamigo/R.java
+++ LunchAmigo/gen/mingle/unimelb/lunchamigo/R.java
@@ -24,9 +24,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_forgot_password=0x7f080009;
-        public static final int action_settings=0x7f08000a;
+        public static final int Button01=0x7f080009;
+        public static final int action_forgot_password=0x7f08000c;
+        public static final int action_settings=0x7f08000d;
         public static final int activityButton=0x7f080008;
+        public static final int button1=0x7f08000b;
+        public static final int button2=0x7f08000a;
         public static final int email=0x7f080003;
         public static final int login=0x7f080005;
         public static final int loginButton=0x7f080007;
